a0aa0fb7063ea063f50a888a5ca535573d477af3
Michael Tarantino <tarantin@adobe.com>
2023-03-23 16:20:15 +0100

    Testing creation of new component carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,17 +1,16 @@
 export default function decorate(block) {
     const slider = document.createElement('div');
-    slider.classList("slider");
+    block.append(slider);
+    slider.classList('slider');
 
     [...block.children].forEach((row) => {
         const slide = document.createElement('div');
-        slide.classList("slide");
         slider.append(slide);
+        slide.classList('slide');
 
         const img = document.createElement('img');
-        img.setAttribute("src", "https://source.unsplash.com/random?landscape,cars");
-        img.setAttribute("alt", "");
         slide.append(img);
+        img.src = "https://source.unsplash.com/random?landscape,cars";
+        img.alt = ""
       });
-
-    block.append(slider);
 }
